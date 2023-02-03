117484e438d9042c53955c64f672853c8fe1f1b6
Chen Peng <chen56@msn.com>
2023-02-03 12:48:17 +0800

    flutter run下载太慢（https://unpkg.com/canvaskit-wasm@0.37.1/bin/canvaskit.wasm），FLUTTER_WEB_CANVASKIT_URL对flutter run不起作，但对build起作用

--- lib/todo.dart
+++ lib/todo.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_key_in_widget_constructors, file_names, prefer_const_constructors
+// ignore_for_file: use_key_in_widget_constructors, file_names, prefer_const_constructors, camel_case_types
 
 import 'package:flutter/widgets.dart';
 
@@ -16,6 +16,29 @@ class TODO1 extends StatelessWidget {
   }
 }
 
-class TODO2{
+class TODO2_navigator{
    //route.settings 如果是Page，会在Page更新时回调Route.xxxx?
+
+}
+
+class TODO3_web_too_slow{
+
+  // 替换canvaskit.wasm CDN 地址，国内defult的太慢，下面方法没用，
+  // 还是从unpkg下载，要五分钟：https://unpkg.com/canvaskit-wasm@0.37.1/bin/canvaskit.wasm
+  //这样不起作用：
+  // flutter build web -v --profile  --web-renderer canvaskit --dart-define=FLUTTER_WEB_CANVASKIT_URL=https://cdn.jsdelivr.net/npm/canvaskit-wasm@0.37.1/bin/ lib/src/notes/navigator/navigator_v2/3.1.navigator_v2_args_screen.dart
+
+  //  flutter run 慢
+  // 拿不到字体：https://fonts.gstatic.com/s/roboto/v20/KFOmCnqEu92Fr1Me5WZLCzYlKw.ttf
+
+  //Flutter 默认会打包 MaterialIcons-Regular.otf 和 CupertinoIcons.ttf 这两种字体库，如何精简
+
+  // html里放一个加载中的element, 在flutter就绪时再删除。
+  // if(kIsWeb) html.document.getElementById("loading")?.remove();
+
+}
+
+
+class TODO_seo{
+  ///低优先级
 }
