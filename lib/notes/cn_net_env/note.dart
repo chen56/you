f675035069e12b3db53e5da41c1f40c8d7e508a0
Chen Peng <chen56@msn.com>
2023-02-03 22:55:40 +0800

    解决字体和wasm下载问题

--- lib/notes/cn_net_env/note.dart
+++ lib/notes/cn_net_env/note.dart
@@ -10,7 +10,7 @@ build() {
   
 ## flutter pub镜像
 
-linux 配置.bashrc:
+解决方案：linux 配置.bashrc:
 
 ```bash
 export PUB_HOSTED_URL=https://pub.flutter-io.cn
@@ -20,10 +20,15 @@ export FLUTTER_STORAGE_BASE_URL=https://storage.flutter-io.cn
 
 ### canvaskit-wasm 下载地址 
 
+web模式 下载不下来：<https://unpkg.com/canvaskit-wasm@0.37.1/bin/canvaskit.wasm>
+可改成：
+- <https://cdn.jsdelivr.net/npm/canvaskit-wasm@0.38.0/bin/canvaskit.wasm>
+- <https://cdnjs.cloudflare.com/ajax/libs/canvaskit-wasm/0.38.0/canvaskit.wasm>
+
 参考文档：<https://docs.flutter.dev/development/platform-integration/web/initialization#initializing-the-engine>
 配置相关源码: <https://github.com/flutter/engine/blob/3.8.0-2.0.pre/lib/web_ui/lib/src/engine/configuration.dart>
 
-#### 方法1：FLUTTER_WEB_CANVASKIT_URL环境变量
+#### 解决方案1：FLUTTER_WEB_CANVASKIT_URL环境变量
 
 ```bash
 # 注意 只有--release环境生效，--debug和--profile都无效 - 2023-02-03 Flutter-3.7.1
@@ -31,24 +36,15 @@ flutter run --device-id chrome --web-renderer canvaskit --release --dart-define=
 
 # build 也有效
 flutter build web -v --release  --web-renderer canvaskit --dart-define=FLUTTER_WEB_CANVASKIT_URL=https://cdn.jsdelivr.net/npm/canvaskit-wasm@0.37.1/bin/ -t lib/notes/navigator/navigator_v2/3.1.navigator_v2_args_screen.dart
-
 ```
 
 
-#### 方法2：定制 /web/index.html
+#### 解决方案2：定制 /web/index.html debug也有效
 
-定制canvasKitBaseUrl
-
-```javascript
-engineInitializer.initializeEngine({
-  canvasKitBaseUrl: "https://cdn.jsdelivr.net/npm/canvaskit-wasm@0.37.1/bin/",
-});
-```
-
-在index.html重的完整样子：
+解决方案：定制canvasKitBaseUrl，在index.html里长这样：
 
 ```html
-<script>
+<script type="text/javascript">
     window.addEventListener('load', function(ev) {
       // Download main.dart.js
       _flutter.loader.loadEntrypoint({
@@ -57,7 +53,8 @@ engineInitializer.initializeEngine({
         },
         onEntrypointLoaded: function(engineInitializer) {
           engineInitializer.initializeEngine({
-            canvasKitBaseUrl: "https://cdn.jsdelivr.net/npm/canvaskit-wasm@0.37.1/bin/",
+            //canvasKitBaseUrl: "https://cdn.jsdelivr.net/npm/canvaskit-wasm@0.38.0/bin/",
+            canvasKitBaseUrl: "https://cdnjs.cloudflare.com/ajax/libs/canvaskit-wasm/0.38.0/",
           }).then(function(appRunner) {
             appRunner.runApp();
           });
@@ -67,12 +64,33 @@ engineInitializer.initializeEngine({
 </script>
 ```
 
-### 字体下载
+### fonts.gstatic.com下载问题
 
-https://fonts.gstatic.com/s/roboto/v20/KFOmCnqEu92Fr1Me5WZLCzYlKw.ttf
-还不知道怎么弄
+下不下来 <https://fonts.gstatic.com/s/roboto/v20/KFOmCnqEu92Fr1Me5WZLCzYlKw.ttf>
 
-硬编码的字体文件地址：_robotoUrl <https://github.com/flutter/engine/blob/3.8.0-2.0.pre/lib/web_ui/lib/src/engine/canvaskit/fonts.dart>
+硬编码的下载地址： <https://github.com/flutter/engine/blob/3.8.0-2.0.pre/lib/web_ui/lib/src/engine/canvaskit/fonts.dart#L21>
+
+解决方案：自己下载到项目目录，塞进pubspec.yaml即可：
+
+```yaml
+flutter:
+  fonts:
+    - family: Roboto
+      fonts:
+        - asset: cache/fonts/KFOmCnqEu92Fr1Me5WZLCzYlKw.ttf
+```
+
+原因：
+
+```dart
+    /// We need a default fallback font for CanvasKit, in order to
+    /// avoid crashing while laying out text with an unregistered font. We chose
+    /// Roboto to match Android.
+    if (!_isFontFamilyDownloaded('Roboto')) {
+      // Download Roboto and add it to the font buffers.
+      _downloadFont(pendingFonts, _robotoUrl, 'Roboto');
+    }
+```
 
   ''');
 
