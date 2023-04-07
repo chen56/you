70d523137f3e4bb146454905e2ce1a488933072a
Chen Peng <chen56@msn.com>
2023-04-07 11:02:50 +0800

    I find a way to inject function info to Mate, issues: #61

--- packages/learn_dart/test/types_test.dart
+++ packages/learn_dart/test/types_test.dart
@@ -55,14 +55,69 @@ void main() {
     });
   });
   group("Function", () {
-    test('f', () {
-      void numberToString(Set<String> selected) {}
-
-      SegmentedButton<String>(
-        segments: [],
-        selected: {},
-        onSelectionChanged: numberToString,
+    test('SpecificTypeFuncInject扩展Function后可在里面加点料，方便mate模型生成代码', () {
+      void myFunc<String>(Set<String> selected) {}
+      var x = SegmentedButton$Mate<String>(
+        onSelectionChanged: myFunc..inject(func: "myFunc"),
       );
+      expect(x.injectInfo, "myFunc");
     });
+    test('AllTypeFuncInject 这样可以区分Function类型： extension X on Function{} ', () {});
   });
+}
+
+class SegmentedButton$Mate<T> {
+  String injectInfo;
+  SegmentedButton$Mate({this.onSelectionChanged}) : injectInfo = "${onSelectionChanged?.name}";
+  void Function<T>(Set<T>)? onSelectionChanged;
+}
+
+extension SpecificTypeFuncInject<T> on void Function<T>(Set<T> selected) {
+  static final _name = Expando<String>();
+  String get name => _name[this] ?? "";
+  set name(String v) => _name[this] = v;
+  inject({
+    required String func,
+  }) {
+    _name[this] = func;
+  }
+}
+
+/// for https://github.com/chen56/note/issues/61
+/// Additional information can be provided for Functions to help Mate code generation
+extension FuncCodeInject on Function {
+  static final _from = Expando<String>();
+  static final _mode = Expando<InjectMode>();
+  String get from => _from[this] ?? "";
+  InjectMode get mode => _mode[this] ?? InjectMode.embed;
+  set name(String v) => _from[this] = v;
+  inject(
+    /// the Mate field Referenced function name
+    String func, {
+    /// expected inject code mode
+    InjectMode mode = InjectMode.refer,
+
+    /// is copy function context code
+    bool copyContext = true,
+  }) {
+    _from[this] = func;
+    _mode[this] = mode;
+  }
+}
+
+enum InjectMode {
+  /// 嵌入模式：会把函数体抽取出来塞到Mate代码生成的构造器中
+  /// void onclick(){}
+  /// Button(onTap:onclick);
+  ///      生成-> Button(onTap:onclick(){});
+  /// 因为会复制外围的变量和函数，所以比较安全，不易出错
+  embed,
+
+  /// 引用模式：会把Mate代码生成的构造器中，只引用函数名
+  /// void onclick(){}
+  /// Button(onTap:onclick);
+  ///      生成-> void onclick(){}
+  ///            Button(onTap:onclick);
+  /// 因为会复制外围的变量和函数，所以比较安全，不易出错
+  refer
 }
