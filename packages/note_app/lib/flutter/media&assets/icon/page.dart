df0d9ec80a7a1f5b114c5f5af652bb8057dfc83c
Chen Peng <chen56@msn.com>
2023-05-07 15:32:48 +0800

    106 web版materialicons regularotf 还是很大12m (#107)
    放弃show all icons的例子，文件太大 1.2M
    * ignore all icons show, file too big

--- packages/note_app/lib/flutter/media&assets/icon/page.dart
+++ packages/note_app/lib/flutter/media&assets/icon/page.dart
@@ -5,8 +5,8 @@ import 'package:note/page_core.dart';
 // [   +4 ms] Font asset "MaterialIcons-Regular.otf" was tree-shaken,
 // reducing it from 1645184 to 10272 bytes (99.4% reduction).
 // Tree-shaking can be disabled by providing the --no-tree-shake-icons flag
-// import 'package:note_mate_flutter/mate_icons.g.dart' as mate_icons;
-import 'package:note_mate_flutter/mate_icons.g.dart' deferred as deferred_icons;
+// import 'package:note_mate_flutter/mate_icons.g.dart' deferred as deferred_icons;
+
 import 'package:note_mate_flutter/material.dart';
 
 NoteConfPart page = NoteConfPart(
@@ -43,41 +43,41 @@ build(BuildContext context, Pen print) {
       ],
     )));
   }
-
-  print.$____________________________________________________________________();
-  print.markdown('''
-## 图标浏览
-
-主图标及扩展图标的范例：
-
-- Icons.delete  (主图标)
-- Icons.delete_sharp (加后缀的扩展图标)
-- Icons.delete_rounded (加后缀的扩展图标)
-- Icons.delete_outlined (加后缀的扩展图标)
-
-下面加载所有图标库，需要几秒钟，请耐心等待：
-  ''');
-  // 按此规律，共有 ${mainIconNames.length}组， 不符合此规律的图标暂未收录.
-  // package:flutter/material/icons.dart 图标库，共图标 ${icons.length}个，大多命名为以下规律：
-  print.$____________________________________________________________________();
-  // why wait 3 seconds? because load library will stuck the ui thread.
-  var iconsFutrue = Future.delayed(const Duration(seconds: 3)).then((value) =>
-      deferred_icons
-          .loadLibrary()
-          .then((value) => deferred_icons.registerIcon()));
-  print(FutureBuilder(
-      future: iconsFutrue,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          if (snapshot.hasError) {
-            return Text(
-                'icons load error :${snapshot.error} \n ${snapshot.stackTrace} ');
-          }
-          var result = iconsWidget(snapshot.data!);
-          return result;
-        }
-        return const CircularProgressIndicator();
-      }));
+// 暂时放弃此范例，因为图标库太大
+//   print.$____________________________________________________________________();
+//   print.markdown('''
+// ## 图标浏览
+//
+// 主图标及扩展图标的范例：
+//
+// - Icons.delete  (主图标)
+// - Icons.delete_sharp (加后缀的扩展图标)
+// - Icons.delete_rounded (加后缀的扩展图标)
+// - Icons.delete_outlined (加后缀的扩展图标)
+//
+// 下面加载所有图标库，需要几秒钟，请耐心等待：
+//   ''');
+//   // 按此规律，共有 ${mainIconNames.length}组， 不符合此规律的图标暂未收录.
+//   // package:flutter/material/icons.dart 图标库，共图标 ${icons.length}个，大多命名为以下规律：
+//   print.$____________________________________________________________________();
+//   // why wait 3 seconds? because load library will stuck the ui thread.
+//   var iconsFutrue = Future.delayed(const Duration(seconds: 3)).then((value) =>
+//       deferred_icons
+//           .loadLibrary()
+//           .then((value) => deferred_icons.registerIcon()));
+//   print(FutureBuilder(
+//       future: iconsFutrue,
+//       builder: (context, snapshot) {
+//         if (snapshot.connectionState == ConnectionState.done) {
+//           if (snapshot.hasError) {
+//             return Text(
+//                 'icons load error :${snapshot.error} \n ${snapshot.stackTrace} ');
+//           }
+//           var result = iconsWidget(snapshot.data!);
+//           return result;
+//         }
+//         return const CircularProgressIndicator();
+//       }));
 }
 
 Widget iconsWidget(IconRegister iconRegister) {
