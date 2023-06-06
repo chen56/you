9f33de253e25d592ffc15b40f4b4fd9a403da4d9
Chen Peng <chen56@msn.com>
2023-06-06 17:50:37 +0800

    fix compile error

--- note/lib/note_shell.dart
+++ note/lib/note_shell.dart
@@ -3,5 +3,4 @@
 // We separate the note_page.dart and note_shell.dart packages of note,
 // because shells can have both flutter and html implementations in the future,
 // and notes should not depend on which shell is used
-export 'package:note/src/note_system.dart' show NoteSystem;
 export 'package:note/src/note_layout.dart' show LayoutScreen, DeferredScreen;
