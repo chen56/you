57b0662036254ab851b811d2fbdbfe8d35ec54e0
Chen Peng <chen56@msn.com>
2023-09-10 17:41:52 +0800

    music_note 页面基本ok

--- spaces/music_note/src/routes/layout.tsx
+++ spaces/music_note/src/routes/layout.tsx
@@ -1,6 +1,6 @@
 import { component$, Slot } from "@builder.io/qwik";
 import type { RequestHandler } from "@builder.io/qwik-city";
-import { Link, useNavigate } from '@builder.io/qwik-city';
+import { Link } from '@builder.io/qwik-city';
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
   // Control caching for this request for best performance and to reduce hosting costs:
