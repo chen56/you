4ae49a19118db5547dc90cb03de7ea2013a019fc
Chen Peng <chen56@msn.com>
2024-03-28 22:59:38 +0800

    bake.bash 移动到新项目：

--- vendor/bake.bash
+++ vendor/bake.bash
@@ -7,51 +7,36 @@ set -o pipefail  # default pipeline status==last command status, If set, status=
 
 _bake_version=v0.3.20240327
 
-# v0.2.20230525 - It can run normally on macos
+# v0.2.20230528 - It can run normally on macos
 # todo
-#   1. 尴尬：当前 无法判断错误命令：./bake no_this_cmd ,因为不知道这是否是此命令的参数，
-#      干脆设一个简单的规则：只有叶子命令才能执行，这样非叶子命令就不需要有参数，好判断了
-#   2. 尴尬：当前 无法判断错误options：./bake --no_this_cmd ,同上
+#   1. 当前 无法判断错误命令：./bake no_this_cmd ,因为不知道这是否是此命令的参数，
+#      需要设置设一个简单的规则：只有叶子命令才能正常执行，这样非叶子命令就不需要有参数
+#   2. 当前 无法判断错误options：./bake --no_this_opt ,同上
 #   3. 类似flutter run [no-]pub 反向选项
 #
-# chinese-----------------------------------------------------------------------
 # bake == (bash)ake == 去Make的bash tool
 # bake 是个简单的命令行工具，以替代Makefile的子命令功能
 # make工具的主要特点是处理文件依赖进行增量编译，但flutter、golang、java、js项目的build工具
 # 太厉害了，这几年唯一还在用Makefile的理由就是他的子命令机制: "make build"、
 # "make run", 可以方便的自定义单一入口的父子命令，但Makefile本身的语法套路也很复杂，
 # 很多批处理还是要靠bash, 这就尴尬了，工具太多，麻烦！本脚本尝试彻底摆脱使用Makefile。
-# 经尝试，代码很少啊 ，核心代码几百行啊，父子命令2百行左右，option解析2百行左右，功能足够了：
+# 经尝试，代码很少啊 ，核心代码几百行啊，父子命令二三百行左右，option解析二三百行左右，功能足够了：
 #
-# english-----------------------------------------------------------------------
-# bake == (bash)ake == De-Make bash tool
-# bake is a simple cli tool to replace the subcommand function of Makefile
-# The main feature of the make tool is to process file dependencies for
-# incremental compilation, but the build tools for flutter, golang, java,
-# and js projects, It's so powerful, the only reason why I still use Makefile
-# in recent years is its subcommand mechanism: "make build",
-# "make run", it is convenient to customize the parent-child command of a
-# single entry, but the syntax of the Makefile itself is also very complicated.
-# A lot of batch processing still depends on bash, which is embarrassing,
-# too many tools, So complicated! This script attempts to get rid of Makefiles
-# altogether. After trying, the code is very little, the core code is
-# hundreds of lines, the parent-child command is about 200 lines,
-# and the option parse is about 200 lines, the function is enough:
-
-# chinese-----------------------------------------------------------------------
 # bake命令规则：
 # 1. 函数即命令，所有bake内的函数均可以在脚本外运行：
 #      ./bake [all function]     # bake内的所有函数均可以在脚本外直接运行
-#      ./bake _self              # 比如这个内部函数, 看bake内部变量，调试脚本用
-#      ./bake test              # 你如果定义过test()函数，就可以这样运行
-# 2. 带"."的函数，形成父子命令，比如 bake.opt()函数是bake.opt()的子命令，bake.opt()
-#    是bake()的子命令，即便未定义父命令，也可以通过父命令列子命令看帮助:
-#      ./bake bake -h           # 运行子命令,或看帮助
-#      ./bake bake opt -h       # 运行子命令,或看帮助
-#      ./bake bake.opt -h   # 运行子命令,或看帮助
-#      ./bake bake.opt -h   # 由于规则1，可直接用函数名运行子命令
-# 3. 你可以定义根命令"_root()"
-#      ./bake                   # 如果有_root()函数，就执行它
+#      ./bake info               # 比如这个内部函数, 看bake内部变量，调试脚本用
+#      ./bake test               # 你如果定义过test()函数，就可以这样运行
+# 2. 带"."的函数，形成父子命令:
+#    web.build(){ echo "build web app"; }
+#    web.test(){ echo "build web app"; }
+#   可以这样调用
+#      ./bake web -h            # 运行子命令,或看帮助
+#      ./bake web.build -h      # 上面等同
+#      ./bake web test -h       # 运行子命令,或看帮助
+#      ./bake web.test -h       # 与上面等同
+# 3. 特殊的root命令表示根命令
+#      ./bake                   # 如果有root()函数，就执行它
 # 4. 像其他高级语言的cli工具一样，用简单变量就可以获取命令option:
 #    # a. 先在bake文件里里定义app options
 #      bake.opt --cmd build --name "target" --type string
@@ -64,53 +49,12 @@ _bake_version=v0.3.20240327
 #      ./bake build --target "macos"
 # 5. bake尽量不依赖bash以外的其他工具，包括linux coreutils,更简单通用,但由于用了关联数组等
 #    依赖bash4+
-# 6. 这个文件copy走，把"bake common script end line."之下的脚本替换成你的就可以用了.
-
-# english-----------------------------------------------------------------------
-# bake rules:
-# 1. bash function are cmd, all functions in bake can be run outside the script:
-#     ./bake [all func]   # All functions in bake can be run outside script
-#     ./bake _self        # For example, this internal function,
-#                         # see bake internal variables, used to debug scripts
-#     ./bake test        # If you have a test() function, you can run it like this
-# 2. Functions with "." is parent-child commands, such as bake.opt() function
-#    is a subcommand of bake.opt(), bake.opt()  is a subcommand of bake(),
-#    even the parent command is not defined, you can also view the help :
-#     ./bake bake -h          # Run subcommands, or see help
-#     ./bake bake opt -h      # Run subcommands, or see help
-#     ./bake bake.opt -h  # Run subcommands, or see help
-#     ./bake bake.opt -h  # Due to rule 1, the subcommand can be run directly with the function name
-# 3. You can define the root command "_root()"
-#     ./bake                  # If there is a _root() function, execute it
-# 4. Like other high-level language cli tools, command options can be obtained with simple variables:
-#     # a. Define options before the command function
-#     bake.opt --cmd build --name "target" --type string
-#     # b. Parse and use option
-#       function build() {
-#         eval "$(bake.parse "${FUNCNAME[0]}" "$@")";
-#         echo "build ... your option: target: $target";
-#       }
-#     # c. Call it:
-#       ./bake build --target "macos"
-# 5. bake try not to depends on too many external tools other than bash,
-#    including linux coreutils, keep it simple, But because of the use of
-#    associative arrays arrays, Depend on bash4+
-# 6. Copy this file, replace the script under "bake common script end line."
-#    with yours and it will be ready to use.
-#
-# other command framework ref:
-# https://pub.dev/documentation/args/latest/args/ArgParser-class.html
-# https://github.com/spf13/pflag
-# https://oclif.io/docs/flags
-#
-# know bash version
-# ref: https://ftp.gnu.org/gnu/bash/
-# 1. ${parameter@operator} : from 2016 bash 4.4
-#      ${parameter@Q} quoted in a format that can be reused as input
-#      ${parameter@E} expanded as with the $'...' quoting mechansim
-#      ${parameter@A} an assignment statement or declare command
-# 2. associative array : from 2009 bash 4.0
-#    declare -A
+# 6. 有两种用法：
+#     - 这个文件copy走，把你的脚本放到本脚本最后即可.
+#     - 在你的脚本里直接curl下载本脚本后 source即可。
+# 范例可以看实际案例：
+#     - https://github.com/chen56/note/blob/main/bake
+#     - https://github.com/chen56/younpc/blob/main/bake
 
 
 # check bake dependencies
