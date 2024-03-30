d7290323dc242a5cc5c1ece4c44dfd68ef2c2abd
Chen Peng <chen56@msn.com>
2024-03-30 11:26:18 +0800

    add spaces/qwik

---
+++ spaces/qwik/src/routes/tailwind/index.tsx
@@ -0,0 +1,13 @@
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+
+export default component$(() => {
+  return (
+    <>
+      <h1>Hi tailwind study</h1>
+      <p>
+
+      </p>
+    </>
+  );
+});
