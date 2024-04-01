52b8abba73250eadd777642647471466a79c1155
Chen Peng <chen56@msn.com>
2024-04-01 21:55:35 +0800

    remote astro music reactjs

--- notes/music_note/src/study/ts_ grammar/ts_core.spec.ts
+++
@@ -1,5 +0,0 @@
-import { test, expect } from "vitest";
-
-test(`[typescrpt grammer] hello`, async () => {
-  expect("hello test").toContain("hello");
-});
--- notes/reactjs_note/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-'use server'
-
-import {redirect} from 'next/navigation'
-
-export default async function ButtonUsage() {
-    redirect(`/workspace`) // Navigate to the new post page
-}
