32674af71ed0f96db7452f227fa130f16ef64efc
Chen Peng <chen56@msn.com>
2024-04-16 20:30:45 +0800

    bugfix：export不存在的类

--- lib/note.dart
+++ lib/note.dart
@@ -1,5 +1,5 @@
 library note;
 
-export 'src/note_core.dart' show NoteSystem, Note, NotePage, NoteContent, Outline, OutlineNode, NoteContentWidgetMixin, Pen, NoteCell, NoteSourceData, DeferredNotePageBuilder, NoteContentExt, NoteContentExtensionPoint, NoteContentArg;
+export 'src/note_core.dart' show NoteSystem, Note, NotePage, NoteContent, Outline, OutlineNode, NoteContentWidgetMixin, Pen, NoteCell, NoteSourceData, DeferredNotePageBuilder, NoteContentExt, NoteContentArg;
 export 'src/content/markdown_content.dart' show MarkdownPenExtension, MockupContent;
 export 'src/content/widget_content.dart' show WidgetContent;
