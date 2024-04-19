4b238cac0fd6bf432920961eada277ff600610e2
Chen Peng <chen56@msn.com>
2024-04-19 09:38:16 +0800

    Unified base-href format for dev and prod environments

--- notes/flutter_web/lib/notes/media&assets&file/file_web/note.dart
+++ notes/flutter_web/lib/notes/media&assets&file/file_web/note.dart
@@ -1,12 +1,9 @@
 // ignore_for_file: file_names
 
 
-import 'package:flutter/widgets.dart' ;
-
+import 'package:flutter/widgets.dart';
 import 'package:you_note_dart/note.dart';
-// web依赖的js_interop 无法在macos平台使用
-// package:flutter_web/main.dart => package:flutter_web/notes.g.dart => package:flutter_web/notes/media&assets&file/file_web/note.dart => package:web/web.dart => package:web/src/dom.dart => package:web/src/dom/angle_instanced_arrays.dart => package:web/src/dom/webgl1.dart => package:web/src/dom/dom.dart => package:web/src/dom/css_font_loading.dart => package:web/src/dom/html.dart => package:web/src/dom/clipboard_apis.dart => package:web/src/dom/fileapi.dart => package:web/src/dom/streams.dart => package:web/src/dom/webidl.dart => dart:js_interop
-// import 'package:web/web.dart' as web;
+import 'package:web/web.dart' as web;
 
 build(BuildContext context, Pen print) {
   print.markdown(r'''
@@ -14,6 +11,6 @@ build(BuildContext context, Pen print) {
   ''');
 
   print("Uri.base(window.location.href): ${Uri.base}");
-  // print("web.window.location.href: ${web.window.location.href};");
+  print("web.window.location.href: ${web.window.location.href};");
 }
 
