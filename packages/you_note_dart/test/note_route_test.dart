9b4fcab4adebb786fd5d21e2cbea8f82f2f5dfcd
Chen Peng <chen56@msn.com>
2024-04-19 15:17:16 +0800

    rebase

---
+++ packages/you_note_dart/test/note_route_test.dart
@@ -0,0 +1,17 @@
+import 'package:collection/collection.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:checks/checks.dart';
+import 'package:path/path.dart' as path;
+void main() {
+  group("Note path", () {
+    test("study", () async {
+      check(path.dirname("/")).equals("/");
+      check(path.dirname("/a/b")).equals("/a");
+      check(path.relative("/a/b",from: "/")).equals("a/b");
+      check(path.dirname("")).equals(".");
+      check(path.basename("/")).equals("/");
+      check(path.basename("")).equals("");
+
+    });
+  });
+}
