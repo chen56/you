ae4cc868bb58f2689c4e79b8eeada9942b598828
Chen Peng <chen56@msn.com>
2024-04-20 11:25:57 +0800

    准备干code解析

--- packages/you_note_dart/test/code_analyzer/use_case_basic.dart
+++ packages/you_note_dart/test/code_analyzer/use_case_basic.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/widgets.dart';
-import 'package:you_note_dart/src/note_cell.dart';
 
 var x = "x";
 
-void build(BuildContext context, Cell print) {
-  // begin comment 中文字符
-  print("x:$x");
-  // end comment 中文字符
+void build(BuildContext context) {
+  debugPrint("x:$x");
 }
 
 class A {}
