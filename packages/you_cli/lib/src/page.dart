db1b3c48fdd93467793f5f2f0c72afc8ad80726e
Chen Peng <chen56@msn.com>
2024-04-25 14:13:31 +0800

    gen pages.g.dart: 2.2 recursive create page node

--- packages/you_cli/lib/src/page.dart
+++ packages/you_cli/lib/src/page.dart
@@ -21,7 +21,12 @@ class PageNode {
 
   bool get isRoot => _parent == this;
 
-  String packageImportUri(String pkgName) => "package:$pkgName/${path.relative(dir.path, from: root.dir.path)}";
+  File get pageDart => dir.childFile("page.dart");
+
+  String packageImportUri(String pkgName,Directory libDir) {
+    var pageDartRelativePath = path.relative(pageDart.path, from: libDir.path);
+    return "package:$pkgName/$pageDartRelativePath";
+  }
 
   String get routePath {
     if(isRoot){
@@ -30,6 +35,26 @@ class PageNode {
     return "/${path.relative(dir.path, from: root.dir.path)}";
   }
 
+  /// note name平整化,可作为变量名：
+  /// lib/pages/1.a/b/page.dart  ---> a_b
+  String get flatName {
+    String p = routePath;
+    if (p == "/") {
+      return "root";
+    }
+    var names = p.split(path.separator).where((e) => e.isNotEmpty);
+    return names
+        .map((e) => e
+    // ignore: unnecessary_string_escapes
+        .replaceAll(RegExp("^\\d+\."), "") // 1.z.about -> note_note-self
+        .replaceAll(".", "_")
+        .replaceAll("-", "_")
+        .replaceAll("&", "_")
+        .replaceAll("*", "_")
+        .replaceAll("*", "_")
+        .replaceAll("@", "_"))
+        .join("_");
+  }
   List<PageNode> toList({
     bool includeThis = true,
     bool Function(PageNode path)? where,
