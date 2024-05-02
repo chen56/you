d9bbb3dc974973c850a0884c040773e6fc709c38
Chen Peng <chen56@msn.com>
2024-05-02 22:10:47 +0800

    clean

--- packages/you_flutter/lib/note.dart
+++ packages/you_flutter/lib/note.dart
@@ -1,7 +1,8 @@
 library note;
 
-export 'src/note/note.dart' show ToNote, Cell, NoteBuilder,NoteLayoutBuilder, NoteSystem;
-export 'src/note/contents/contents.dart' show contents;
+export 'src/note/note.dart' show ToNote, Cell, NoteBuilder, NoteLayoutBuilder, NoteSystem;
+
+export 'src/note/contents/contents.dart' show contents, Contents;
 export 'src/note/contents/mockup.dart' show MockupWindow;
 export 'src/note/contents/markdown_content.dart' show MD;
 
