9decfefc0010bfd654a95e290dc41101ce4077e6
Chen Peng <chen56@msn.com>
2024-05-13 02:00:11 +0800

    [flutter_web] update to : Flutter 3.22.0-31.0.pre.20 • channel master

--- notes/flutter_web/lib/routes/notes/media&assets&file/assets/page.dart
+++ notes/flutter_web/lib/routes/notes/media&assets&file/assets/page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:you_flutter/note.dart';
-import 'package:flutter/services.dart' show rootBundle;
 
 void build(BuildContext context, Cell print) async {
   print.addCell(title:const Text("--------new cell------"));
