89640af90ffed2f748fc6567348e8d37470713b7
Chen Peng <chen56@msn.com>
2024-06-24 21:37:54 +0800

    [learn_dart]  Create Stream: async*

--- notes/learn_dart/test/dart_lang!sdk!io/stream_test.dart
+++ notes/learn_dart/test/dart_lang!sdk!io/stream_test.dart
@@ -3,20 +3,31 @@
 import 'package:checks/checks.dart';
 import 'package:test/test.dart';
 
-
 main() async {
-  group('Create Stream: async*', () {
+  group('Create Stream', () {
     test('create', () async {
       Stream<int> range(int to) async* {
         for (int i = 1; i <= to; i++) {
           yield i;
         }
       }
+
       List<int> collect = [];
       await for (int i in range(3)) {
         collect.add(i);
       }
       check(collect).deepEquals([1, 2, 3]);
     });
+
+    test('Stream<int>.fromIterable()', () async {
+      var s = Stream<int>.fromIterable([2, 4, 6]);
+      check(await s.toList()).deepEquals([2, 4, 6]);
+    });
+    test('Stream<int>.periodic()', () async {
+      var counterStream = Stream<int>.periodic(const Duration(seconds: 1), (x) => x).take(3);
+      check(await counterStream.toList()).deepEquals([0, 1, 2]);
+    });
   });
+
+  group('Use Stream', () {});
 }
