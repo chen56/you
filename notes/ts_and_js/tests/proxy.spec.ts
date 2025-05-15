dda8ace0a2cc2136a324f832f0d304569467e809
Chen Peng <chen56@msn.com>
2025-05-15 11:18:10 +0800

    add study_ts_and_js

---
+++ notes/ts_and_js/tests/proxy.spec.ts
@@ -0,0 +1,9 @@
+   import { describe, it } from 'vitest';
+   import assert from 'node:assert';
+
+   describe('example', () => {
+     it('synchronous passing test', () => {
+       assert.strictEqual(1, 1);
+     });
+   });
+   
